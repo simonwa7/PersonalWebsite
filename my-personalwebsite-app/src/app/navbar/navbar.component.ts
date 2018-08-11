160827bcd8a6374c5f6a8e997c52bc1078d55144
simonwa7 <simonwa7@gmail.com>
2018-08-11 15:01:56 -0400

    Adding Bootstrap Theme

--- my-personalwebsite-app/src/app/navbar/navbar.component.ts
+++ my-personalwebsite-app/src/app/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css',
+  			  './../../assets/css/grayscale.css']
 })
 export class NavbarComponent implements OnInit {
 
