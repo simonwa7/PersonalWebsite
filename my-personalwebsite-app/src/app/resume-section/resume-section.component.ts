df97fa68bc1b159d728c9576e2c844f2ab0b500a
simonwa7 <simonwa7@gmail.com>
2018-08-11 22:43:24 -0400

    Finished Resume template

--- my-personalwebsite-app/src/app/resume-section/resume-section.component.ts
+++ my-personalwebsite-app/src/app/resume-section/resume-section.component.ts
@@ -7,10 +7,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ResumeSectionComponent implements OnInit {
 	@Input('title')title: string;
+	@Input('show')show: boolean;
 
 	constructor() { }
 
 	ngOnInit() {
+	}
+
+	toggle_show(){
+		if(this.show){
+			this.show = false;
+		}else{
+			this.show = true;
+		}
 	}
 
 }
