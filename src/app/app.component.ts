13221d74e04dc10395027933f4a242d3fb4a5729
simonwa7 <simonwa7@gmail.com>
2018-08-21 00:20:53 -0400

    Changed bio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   firstname:string = 'William';
   lastname:string =  'Simon';
-  bio = 'I am a recent graduate from Tufts University with a passion for understanding and developing quantum algorithms'
+  bio = 'I am a recent graduate from Tufts University with a passion for understanding and developing algorithms for quantum computation'
 }
