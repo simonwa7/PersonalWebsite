e0eb09ae0dd65c0cd27aed9122a11ebebc6aac51
simonwa7 <simonwa7@gmail.com>
2018-12-29 11:22:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,10 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/my-personalwebsite-app/index.html'));
 });
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://william-simon.herokuapp.com");
+}, 1500000); // every 25 minutes (1500000)
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
