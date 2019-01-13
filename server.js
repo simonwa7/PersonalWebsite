928020e11878ea010ebe86536ae0038b1644b5c4
simonwa7 <simonwa7@gmail.com>
2019-01-13 15:55:20 -0500

    classifier stuff

--- server.js
+++ server.js
@@ -9,7 +9,34 @@ app.use(express.static(__dirname + '/dist/my-personalwebsite-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/my-personalwebsite-app/index.html'));
+	res.sendFile(path.join(__dirname+'/dist/my-personalwebsite-app/index.html'));
+});
+
+
+
+var fs = require('fs');
+
+app.get('/classifier', function(req,res) {
+
+	try {  
+	    var data = fs.readFileSync('training.txt', 'utf8');
+	    console.log(data.length)
+	    var i=0;
+	    var size=data.length;
+
+	    while(i<size){
+	        while(data[i]!="    "){
+	            console.log(data[i])
+	            ++i;
+	        }
+	        break;
+	    }
+
+	    // console.log(data.toString());    
+	} catch(e) {
+	    console.log('Error:', e.stack);
+	}
+
 });
 
 var http = require("http");
